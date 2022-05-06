910643a7b67fe8e31f7aa109e361d754d21a054a
aditore <anthonyditore@gmail.com>
2022-05-06 10:27:10 -0700

    homepage layout set, needs styling

--- src/components/home.js
+++ src/components/home.js
@@ -3,13 +3,13 @@ import React from "react";
 function Home() {
     return(
         <div className="homePage px-2">
-        <section className="homePage col-8">
-            <p>Hello! My name is Anthony Ditore! Welcome to my website!</p>
-
-            <p>Please read about me and take a gander at my works!</p>
-
-            <p>Feel free to contact me if you have an opportunity or would like to collaborate!</p>
-        </section>
+            <h1 className="nameHeader">Anthony Ditore</h1>
+            <img className="twinsImage" alt="twins" src="./images/twins.jpg"></img> 
+            <section className="homePage col-9">
+                <p>Hello! My name is Anthony Ditore! Welcome to my website!</p>
+                <p>Please read <a href="/aboutMe">about me</a> and take a gander at my <a href="/myWork">works</a>!</p>
+                <p>Feel free to <a href="/contact">contact me</a> if you have an opportunity or would like to collaborate!</p>
+            </section>
         </div>
     )
 }
