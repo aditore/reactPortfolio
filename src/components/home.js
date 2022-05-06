43c58818287e45a9a8c3540a2e5c856d8ae8cf9e
aditore <anthonyditore@gmail.com>
2022-05-05 21:23:42 -0700

    working on basic form

--- src/components/home.js
+++ src/components/home.js
@@ -2,9 +2,15 @@ import React from "react";
 
 function Home() {
     return(
-        <section className="homePage">
-            Hello! My name is Anthony Ditore!
+        <div className="homePage px-2">
+        <section className="homePage col-8">
+            <p>Hello! My name is Anthony Ditore! Welcome to my website!</p>
+
+            <p>Please read about me and take a gander at my works!</p>
+
+            <p>Feel free to contact me if you have an opportunity or would like to collaborate!</p>
         </section>
+        </div>
     )
 }
 
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,24 +4,20 @@ import { NavLink } from 'react-router-dom';
 
 function NavbarTop(props) {
     return (
-        <div className='topNav'>
-            <nav className='navbar'>
-                <div className='container'>             
-                    <NavLink className='navbar-brand' to='/'>Anthony Ditore</NavLink>
-                    <ul>
-                        <li className='nav-item'>
-                            <NavLink className='nav-link' to='/aboutMe'>About Me</NavLink>
-                        </li>
-                        <li className='nav-item'>
-                            <NavLink className='nav-link' to='/myWork'>My Work</NavLink>
-                        </li>
-                        <li className='nav-item'>
-                            <NavLink className='nav-link' to='/contact'>Contact</NavLink>
-                        </li>                        
-                    </ul>
-                </div>
-            </nav>
-        </div>
+        <nav className='navbar navbar-expand-lg justify-content-between'>             
+            <NavLink className='navbar-brand' to='/'>Anthony Ditore</NavLink>
+                <ul className='navbar-nav'>
+                    <li className='nav-item px-2'>
+                        <NavLink className='nav-link' to='/aboutMe'>About Me</NavLink>
+                    </li>
+                    <li className='nav-item px-2'>
+                        <NavLink className='nav-link' to='/myWork'>My Work</NavLink>
+                    </li>
+                    <li className='nav-item px-2'>
+                        <NavLink className='nav-link' to='/contact'>Contact</NavLink>
+                    </li>                        
+                </ul>
+        </nav>
     );
 }
 
