2986de37b9b619ad5336b61414b2a10d5a2d475b
aditore <anthonyditore@gmail.com>
2022-05-09 12:25:04 -0700

    hash router in different spot

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 //import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import {
   NavbarTop,
   Home,
