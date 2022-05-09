9055d7b431e847c692db806b4b4c0ae95dcc4251
aditore <anthonyditore@gmail.com>
2022-05-09 12:29:06 -0700

    changing routes back but with hash router

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,19 +5,19 @@ import { NavLink } from 'react-router-dom';
 function NavbarTop(props) {
     return (
         <nav className='navbar navbar-expand-lg justify-content-between'>             
-            <NavLink className='navbar-brand' to='/reactPortfolio/'>Anthony Ditore</NavLink>
+            <NavLink className='navbar-brand' to='/'>Anthony Ditore</NavLink>
                 <ul className='navbar-nav'>
                     <li className='nav-item px-2'>
-                        <NavLink className='nav-link' to='/reactPortfolio/aboutMe'>About Me</NavLink>
+                        <NavLink className='nav-link' to='/aboutMe'>About Me</NavLink>
                     </li>
                     <li className='nav-item px-2'>
-                        <NavLink className='nav-link' to='/reactPortfolio/myWork'>Portfolio</NavLink>
+                        <NavLink className='nav-link' to='/myWork'>Portfolio</NavLink>
                     </li>
                     <li className='nav-item px-2'>
-                        <NavLink className='nav-link' to='/reactPortfolio/contact'>Contact</NavLink>
+                        <NavLink className='nav-link' to='/contact'>Contact</NavLink>
                     </li>
                     <li className='nav-item px-2'>
-                        <NavLink className='nav-link' to='/reactPortfolio/resume'>Resume</NavLink>
+                        <NavLink className='nav-link' to='/resume'>Resume</NavLink>
                     </li>                        
                 </ul>
         </nav>
