6823e5c7f7df58b1c92b9213eabd4d34016eea9b
aditore <anthonyditore@gmail.com>
2022-05-09 12:21:40 -0700

    trying hash router

--- src/App.js
+++ src/App.js
@@ -22,10 +22,10 @@ function App() {
       <main>
       <Routes>
         <Route path="/reactPortfolio/" element={<Home />} />
-        <Route path="/aboutMe" element={<AboutMePage />} />
-        <Route path="/myWork" element={<MyWork />} />
-        <Route path="/contact" element={<ContactForm />} />
-        <Route path="/resume" element={<ResumePage />} />
+        <Route path="/reactPortfolio/aboutMe" element={<AboutMePage />} />
+        <Route path="/reactPortfolio/myWork" element={<MyWork />} />
+        <Route path="/reactPortfolio/contact" element={<ContactForm />} />
+        <Route path="/reactPortfolio/resume" element={<ResumePage />} />
       </Routes>
       </main>
       <Footer />
--- src/components/home.js
+++ src/components/home.js
@@ -6,8 +6,8 @@ function Home() {
             <img className="twinsImage" alt="twins" src="./images/jpg/twins.jpg"></img> 
             <section className="homePage col-9">
                 <p>Hello! My name is Anthony Ditore! Welcome to my website!</p>
-                <p>Please read <a href="/aboutMe">about me</a> and take a gander at my <a href="/myWork">works</a>!</p>
-                <p>Feel free to <a href="/contact">contact me</a> if you have an opportunity or would like to collaborate!</p>
+                <p>Please read <a href="/reactPortfolio/aboutMe">about me</a> and take a gander at my <a href="/reactPortfolio/myWork">works</a>!</p>
+                <p>Feel free to <a href="/reactPortfolio/contact">contact me</a> if you have an opportunity or would like to collaborate!</p>
             </section>
         </div>
     )
